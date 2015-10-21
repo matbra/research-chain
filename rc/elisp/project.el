3ecba347ca289b3a3d595d415ddb7571b2ccccf6
Matthias Brandt <der.matthias.brandt@gmail.com>
2015-10-21 20:47:30 +0200

    Basic menu functionality working

--- rc/elisp/project.el
+++ rc/elisp/project.el
@@ -1,7 +1,39 @@
+;; for the menu i learned quite a lot from buff-menu.el
+;; (http://www.mit.edu/afs.new/athena/astaff/source/src-8.2/third/emacs/lisp/buff-menu.el)
+
 (require 'json)
+(require 'widget)
 
-(setq debug-on-error t)
+(setq path-seperator '"/")
+(setq settings-file-name '"settings.json")
 
+;; setup the keymap
+(setq rc-menu-mode-map (make-keymap))
+(suppress-keymap rc-menu-mode-map t)
+(define-key rc-menu-mode-map "q" 'rc-menu-quit)
+(define-key rc-menu-mode-map "\C-m" 'rc-menu-entry)
+
+
+(defun rc-menu-quit ()
+  "Quit the menu."
+  (interactive)
+  (let ((buffer (current-buffer)))
+    ;; Switch away from the buffer menu and bury it.
+    (switch-to-buffer (other-buffer))
+    (bury-buffer buffer)))
+
+
+(defun rc-menu-entry ()
+  "Select this line's buffer in this window."
+  (interactive)
+  (message (current-menu-entry)))
+
+
+(defun current-menu-entry ()
+  "Return buffer described by this line of buffer menu."
+(buffer-substring (point-at-bol) (point-at-eol)))
+
+;; simply prints a list
 (defun print-elements-of-list (list)
   "Print each element of LIST on a line of its own."
   (if list
@@ -10,96 +42,55 @@
     (setq list (cdr list)))
   (message "the list is empty.")))
 
-(setq path-seperator '"/")
-(setq settings-file-name '"settings.json")
-
-;;(json-read-file '"../../settings.json")
 
 ;; function to find the path to the next (upwards in the directory tree)
 ;; settings file.
 (defun find-next-settings-file ()
-;;(interactive)
-(setq cur-dir (buffer-file-name))
+  (setq cur-dir (buffer-file-name))
 
-;;(message cur-dir)
+  (setq idx-sep  (length cur-dir))
+  (setq b-stop-looping nil)
+  (while (and (> idx-sep 0) (not b-stop-looping))
+    ;; find the last path seperator in the path string
+    (setq idx-sep 0)
+    (setq read-pos 0)
+    
+    (while (< read-pos (- (length cur-dir) 1))
+      (if (string= (substring cur-dir read-pos (+ read-pos 1)) path-seperator)
+	  (setq idx-sep read-pos))
 
-(setq idx-sep  (length cur-dir))
-;;(message (number-to-string cur-pos))
-(setq b-stop-looping nil)
-(while (and (> idx-sep 0) (not b-stop-looping))
-  ;; find the last path seperator in the path string
-  (setq idx-sep 0)
-  (setq read-pos 0)
-  
-  (while (< read-pos (- (length cur-dir) 1))
-    (if (string= (substring cur-dir read-pos (+ read-pos 1)) path-seperator)
-	;(message "bang")
-	(setq idx-sep read-pos)
-;;      (message (number-to-string idx-sep))
-      )
+      (setq read-pos (1+ read-pos)))
 
-    (setq read-pos (1+ read-pos))
-    ;(message (number-to-string read-pos))
-    )
-
-  ;;(message (concat "/ at " (number-to-string idx-sep)))
-  ;;  (message cur-dir)
-;;  (message (number-to-string idx-sep))
-  (setq cur-pos idx-sep);; (length cur-dir))
-  (setq cur-dir (substring cur-dir 0 (1+ idx-sep)))
-  ;;(message cur-dir)
-;;  (message "bla")
-  ;;  (setq last-sep '(search-backward 
-  ;;(print cur-pos)
-  ;;  (message cur-pos)
-  ;;  (setq cur-dir '(substring cur-dir 1 2))
-;;  (message (number-to-string idx-sep))
-  ;;(setq cur-pos (- cur-pos 1))
-(setq candidate-path (concat cur-dir settings-file-name))
-  (when (file-exists-p candidate-path)
+    (setq cur-pos idx-sep)
+    (setq cur-dir (substring cur-dir 0 (1+ idx-sep)))
+    (setq candidate-path (concat cur-dir settings-file-name))
+    (when (file-exists-p candidate-path)
       (setq b-stop-looping t)))
 
-(if b-stop-looping cur-dir nil))
-;      (return find-next-settings-file (candidate-path)) nil)))
+  (if b-stop-looping cur-dir nil))
+
 
 (defun read-settings-file (filepath)
   (setq settings (json-read-file filepath)))
 
 (defun print-alist (list)
-;;(setq settings)
-(while list
-  (setq cur-element (car list))
-  (setq cur-key (car cur-element))
-  (setq cur-value (cdr cur-element))
-  (message (format "%10s: %s" cur-key cur-value))
-  (setq list (cdr list))
-)
-)
+  (while list
+    (setq cur-element (car list))
+    (setq cur-key (car cur-element))
+    (setq cur-value (cdr cur-element))
+    (message (format "%10s: %s" cur-key cur-value))
+    (setq list (cdr list))))
 
-;;(setq settings ())
-;;(load-next-settings-file)
-;;(message (load-next-settings-file))
-;;(message settings)
-
-(message (find-next-settings-file))
-
-;;(print-alist settings)
-;;(print-alist settings)
-
-
-;(print-alist (cons (assoc 'name settings) '()))
 
 (defun find-all-inbox-entries ()
   ;; get the project directory
-  ;;(setq settings (load-next-settings-file))
   (setq dir-project-root (find-next-settings-file))
   (setq dir-inbox (concat dir-project-root "inbox/"))
   (message dir-inbox)
-  ;; traverse through the subdirectories
-  (message (directory-files-and-attributes dir-inbox))
-  )
 
-;;(find-all-inbox-entries)
+  ;; traverse through the subdirectories
+  (message (directory-files-and-attributes dir-inbox)))
+
 
 (defun find-files-and-directories (dirname)
   (let (cur-files '() all-files-and-dirs '())
@@ -131,28 +122,55 @@
 
   (dolist (new-file new-files)
     (message (concat "path: " dirname ", file:" new-file)))
-;    (add-to-list 'all-files-and-dirs (concat (file-name-as-directory dirname) new-file)))
-;    (add-to-list 'all-files-and-dirs (concat 
     (add-to-list 'all-files-and-dirs `((dir . ,dirname) (file . ,new-files)))
 
-;(message "stored everything")
-
-
-;    (message all-files-and-dirs)
   all-files-and-dirs))
-
-
-
-
 
 (setq dir-project-root (find-next-settings-file))
 (setq dir-inbox (concat dir-project-root "inbox/"))
-;(print-elements-of-list (find-files-and-directories dir-inbox))
 (setq dirs-and-files (find-files-and-directories dir-inbox))
 
-;(print-elements-of-list (cdr (car dirs-and-files)))
 
+(defun create-file-selection-form (files-and-dirs)
+  (switch-to-buffer "*Inbox Selection*")
+  (kill-all-local-variables)
+  (setq buffer-read-only nil)
+  (let ((inhibit-read-only t))
+    (erase-buffer))
+  (remove-overlays)
 
+  ;; write the names of all directories and files into the buffer
+  (while files-and-dirs
+    (setq cur-entry (car files-and-dirs))
+    (message (cdr (assoc 'dir cur-entry)))
+    (setq cur-dirname (assoc 'dir cur-entry))
+    (setq cur-files (cdr (assoc 'file cur-entry)))
+
+    (insert (cdr (assoc 'dir cur-entry)))
+    (insert "\n")
+    (insert (make-string 20 ?-))
+    (insert "\n")
+
+    ;; add the files to the current dir
+    (while cur-files
+      (setq cur-file (car cur-files))
+      (insert cur-file)
+
+      (setq cur-files (cdr cur-files))
+
+      (if cur-files
+	  (insert "\n")))
+
+    (setq files-and-dirs (cdr files-and-dirs))
+    (if files-and-dirs
+	(insert (make-string 2 ?\n))))
+
+  (setq buffer-read-only t)
+
+  (interactive "p")
+  (use-local-map rc-menu-mode-map))
+
+(create-file-selection-form (find-files-and-directories dir-inbox))
 
 
 
