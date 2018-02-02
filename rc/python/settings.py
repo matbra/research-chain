480925f5e515d2770cddd24cf8e87bd6d15ea90a
Matthias Brandt <der.matthias.brandt@gmail.com>
2018-02-02 10:56:09 +0100

    Update the default theme settings.

--- rc/python/settings.py
+++ rc/python/settings.py
@@ -24,13 +24,14 @@ grid = importr("grid")
 #             'panel.border': ggplot.element_blank()}
 
 theme_base = {'legend.background': ggplot.element_rect(color="black", size = 0.1, fill=scales.alpha("white", 0.8)),
-            'legend.key': ggplot.element_blank(),
-            'legend.justification': robj.r.c(1, 1),
-            'legend.position': robj.r.c(0.9, 0.9),
-            'panel.border': ggplot.element_rect(fill=robj.r("NA"), color="black", size=0.5, linetype="solid"),
-            'panel.background': ggplot.element_blank(),
-            'panel.grid.major': ggplot.element_line(colour="grey65"),
-            'panel.grid.minor': ggplot.element_line(colour="grey65")}
+              'legend.key': ggplot.element_blank(),
+              'legend.justification': robj.r.c(1, 1),
+              'legend.position': robj.r.c(0.9, 0.9),
+              'panel.border': ggplot.element_rect(fill=robj.r("NA"), color="black", size=0.5, linetype="solid"),
+              'panel.background': ggplot.element_blank(),
+              'panel.grid.major': ggplot.element_line(colour="grey65"),
+              'panel.grid.minor': ggplot.element_line(colour="grey65"),
+              'plot.margin': grid.unit(robj.r.c(0, 0, 0, 0), "cm")}
             #'panel.border': ggplot.element_blank()}
 # 'panel.spacing': grid.unit(robj.FloatVector((1, 2, 3, 4)), "cm"),
 
